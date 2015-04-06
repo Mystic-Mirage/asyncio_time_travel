705e657b6e492c6a5c7af3230b6e7f952d34ac2a
real <realcr@gmail.com>
2015-04-06 18:16:09 +0300

    Added initial TimeTravelLoop implementation.

---
+++ test_time_travel_loop.py
@@ -0,0 +1,91 @@
+import pytest
+import asyncio
+
+from .time_travel_util import TimeTravelLoop
+
+
+def run_until_timeout(cor,loop,timeout=None):
+    """
+    Run a given coroutine with timeout.
+    """
+    task_with_timeout = asyncio.wait_for(cor,timeout,loop=loop)
+    loop.run_until_complete(task_with_timeout)
+
+
+def test_time_travel_loop_basic_timeout():
+    """
+    Sleep a for a long time. Expect timeout when not enough time is given for
+    the coroutine.
+    """
+
+    SLEEP_TIME = 0x1000
+    NUM_SLEEPS = 5
+
+    tloop = TimeTravelLoop()
+
+    @asyncio.coroutine
+    def inner_cor():
+        for i in range(NUM_SLEEPS):
+            yield from asyncio.sleep(SLEEP_TIME,loop=tloop)
+
+    # Expected time for running:
+    total_time = SLEEP_TIME * NUM_SLEEPS
+
+    # This should work correctly:
+    tloop.run_until_complete(asyncio.wait_for(\
+            inner_cor(),timeout=total_time + 1,loop=tloop))
+
+    run_until_timeout(\
+            inner_cor(),loop=tloop,timeout=total_time + 1)
+
+    with pytest.raises(asyncio.futures.TimeoutError):
+        run_until_timeout(inner_cor(),loop=tloop,timeout=total_time - 1)
+    tloop.close()
+
+
+def test_time_travel_loop_concurrent_sleep():
+    """
+    Create a few tasks that finish at different times.
+    Expect the finish times to be of specific order.
+    """
+
+    # A long sleeping time:
+    SLEEP_TIME = 0x1000
+
+    # results list:
+    res_list = []
+
+    def add_res(res):
+        """Add a result"""
+        res_list.append(res)
+
+    tloop = TimeTravelLoop()
+
+    @asyncio.coroutine
+    def inner_cor():
+        # Add result 0:
+        add_res(0)
+
+        task1 = asyncio.async(asyncio.sleep(delay=SLEEP_TIME*3,loop=tloop),loop=tloop)
+        task2 = asyncio.async(asyncio.sleep(delay=SLEEP_TIME*1,loop=tloop),loop=tloop)
+        task3 = asyncio.async(asyncio.sleep(delay=SLEEP_TIME*4,loop=tloop),loop=tloop)
+        task4 = asyncio.async(asyncio.sleep(delay=SLEEP_TIME*2,loop=tloop),loop=tloop)
+
+        task1.add_done_callback(lambda x:add_res(1))
+        task2.add_done_callback(lambda x:add_res(2))
+        task3.add_done_callback(lambda x:add_res(3))
+        task4.add_done_callback(lambda x:add_res(4))
+
+        tasks = [task1,task2,task3,task4]
+
+        # Wait for all the tasks to complete:
+        yield from asyncio.wait(tasks,loop=tloop,timeout=None)
+
+    # Expected time for running:
+    total_time = (SLEEP_TIME * 4) + 1
+
+    run_until_timeout(inner_cor(),loop=tloop,timeout=total_time)
+    tloop.close()
+
+    assert res_list == [0,2,4,1,3]
+
---
+++ time_travel_util.py
@@ -0,0 +1,129 @@
+from asyncio import base_events,selectors
+from asyncio.test_utils import TestSelector
+import collections
+import heapq
+
+
+# A class to manage set of next events:
+class NextTimers:
+    def __init__(self):
+        # Timers set. Used to check uniqueness:
+        self._timers_set = set()
+        # Timers heap. Used to get the closest timer event:
+        self._timers_heap = []
+
+    def add(self,when):
+        """
+        Add a timer (Future event).
+        """
+        # We don't add a time twice:
+        if when in self._timers_set:
+            return
+
+        # Add to set:
+        self._timers_set.add(when)
+        # Add to heap:
+        heapq.heappush(self._timers_heap,when)
+
+    def is_empty(self):
+        return (len(self._timers_set) == 0)
+
+
+    def pop_closest(self):
+        """
+        Get closest event timer. (The one that will happen the soonest).
+        """
+        if self.is_empty():
+            raise IndexError('NextTimers is empty')
+
+        when = heapq.heappop(self._timers_heap)
+        self._timers_set.remove(when)
+
+        return when
+
+# Based on TestLoop from asyncio.test_utils:
+class TimeTravelLoop(base_events.BaseEventLoop):
+    """
+    Loop for unittests. Passes time without waiting, but makes sure events
+    happen in the correct order.
+    """
+
+    def __init__(self):
+        super().__init__()
+
+        self._time = 0
+        self._clock_resolution = 1e-9
+        self._timers = NextTimers()
+        self._selector = TestSelector()
+
+        self.readers = {}
+        self.writers = {}
+        self.reset_counters()
+
+    def time(self):
+        return self._time
+
+    def advance_time(self, advance):
+        """Move test time forward."""
+        if advance:
+            self._time += advance
+
+    def add_reader(self, fd, callback, *args):
+        self.readers[fd] = events.Handle(callback, args, self)
+
+    def remove_reader(self, fd):
+        self.remove_reader_count[fd] += 1
+        if fd in self.readers:
+            del self.readers[fd]
+            return True
+        else:
+            return False
+
+    def assert_reader(self, fd, callback, *args):
+        assert fd in self.readers, 'fd {} is not registered'.format(fd)
+        handle = self.readers[fd]
+        assert handle._callback == callback, '{!r} != {!r}'.format(
+            handle._callback, callback)
+        assert handle._args == args, '{!r} != {!r}'.format(
+            handle._args, args)
+
+    def add_writer(self, fd, callback, *args):
+        self.writers[fd] = events.Handle(callback, args, self)
+
+    def remove_writer(self, fd):
+        self.remove_writer_count[fd] += 1
+        if fd in self.writers:
+            del self.writers[fd]
+            return True
+        else:
+            return False
+
+    def assert_writer(self, fd, callback, *args):
+        assert fd in self.writers, 'fd {} is not registered'.format(fd)
+        handle = self.writers[fd]
+        assert handle._callback == callback, '{!r} != {!r}'.format(
+            handle._callback, callback)
+        assert handle._args == args, '{!r} != {!r}'.format(
+            handle._args, args)
+
+    def reset_counters(self):
+        self.remove_reader_count = collections.defaultdict(int)
+        self.remove_writer_count = collections.defaultdict(int)
+
+    def _run_once(self):
+        super()._run_once()
+        # Advance time only when we finished everything at the present:
+        if len(self._ready) == 0:
+            if not self._timers.is_empty():
+                self._time = self._timers.pop_closest()
+                # print('time:',self._time,'timers:',self._timers._timers_set)
+
+    def call_at(self, when, callback, *args):
+        self._timers.add(when)
+        return super().call_at(when, callback, *args)
+
+    def _process_events(self, event_list):
+        return
+
+    def _write_to_self(self):
+        pass
