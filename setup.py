4d73cb610db7ac1098c44c525aff2bf9b6e49981
real <realcr@gmail.com>
2015-04-07 19:29:57 +0300

    Importing version from projects and cosmetics.

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
-"""A setuptools based setup module.
-"""
-
 # Always prefer setuptools over distutils
 from setuptools import setup, find_packages
 # To use a consistent encoding
 from codecs import open
 from os import path
+
+# Get the version 
+from .asyncio_time_travel.version import version
 
 here = path.abspath(path.dirname(__file__))
 
@@ -13,13 +13,17 @@ here = path.abspath(path.dirname(__file__))
 with open(path.join(here, 'README.md'), encoding='utf-8') as f:
     long_description = f.read()
 
+py_version = sys.version_info[:2]
+if py_version < (3,3):
+    raise Exception('asyncio_time_travel requires Python >= 3.3.')
+
 setup(
     name='asyncio_time_travel',
 
     # Versions should comply with PEP440.  For a discussion on single-sourcing
     # the version across setup.py and the project code, see
     # https://packaging.python.org/en/latest/single_source_version.html
-    version='0.1.0a0',
+    version=version,
 
     description='Asyncio Time Travel Loop',
     long_description=long_description,
@@ -54,45 +58,14 @@ setup(
         'Programming Language :: Python :: 3.4',
     ],
 
-    # What does your project relate to?
     keywords='asyncio testing time travel sleep',
-
     # You can just specify the packages manually here if your project is
     # simple. Or you can use find_packages().
     packages=find_packages(exclude=['contrib', 'docs', 'tests*']),
+    extras_require={
+            ':python_version=="3.3"': ['asyncio'],
+    },
 
-    # List run-time dependencies here.  These will be installed by pip when
-    # your project is installed. For an analysis of "install_requires" vs pip's
-    # requirements files see:
-    # https://packaging.python.org/en/latest/requirements.html
-    install_requires=[],
-
-    # List additional groups of dependencies here (e.g. development
-    # dependencies). You can install these using the following syntax,
-    # for example:
-    # $ pip install -e .[dev,test]
-    extras_require={},
-    #     'dev': ['check-manifest'],
-    #     'test': ['coverage'],
-    # },
-
-    # If there are data files included in your packages that need to be
-    # installed, specify them here.  If using Python 2.6 or less, then these
-    # have to be included in MANIFEST.in as well.
-    package_data={},
-    #     'sample': ['package_data.dat'],
-    # },
-
-    # Although 'package_data' is the preferred approach, in some case you may
-    # need to place data files outside of your packages. See:
-    # http://docs.python.org/3.4/distutils/setupscript.html#installing-additional-files # noqa
-    # In this case, 'data_file' will be installed into '<sys.prefix>/my_data'
-    # data_files=[('my_data', ['data/data_file'])],
-    data_files = [],
-
-    # To provide executable scripts, use entry points in preference to the
-    # "scripts" keyword. Entry points provide cross-platform support and allow
-    # pip to create the appropriate form of executable for the target platform.
     entry_points={
         'console_scripts': [
             'sample=sample:main',
